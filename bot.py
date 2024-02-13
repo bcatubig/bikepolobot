320137d47eec2d0007ebfa3cf146f4c459d55c07
Brandon Catubig <bcatubig@pm.me>
2024-02-13 15:08:18 -0800

    add help and check perms on queue clear

--- bot.py
+++ bot.py
@@ -16,6 +16,13 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.command(name="raise")
 async def _raise(ctx: Context, *, arg=None):
     match arg:
+        case "help":
+            msg = "Here are some commands you can run\n"
+            msg += "- `!raise` - add yourself to the queue\n"
+            msg += "- `!raise list` - list the current queue\n"
+            msg += "- `!raise next` - get the next person up from the queue - _Admin/Mods only_\n"
+            msg += "- `!raise clear` - clear the queue -  _Admin/Mods only_\n"
+            await ctx.send(msg)
         case "list":
             print("Listing queue")
             msg = "Here is the current queue:\n"
@@ -36,12 +43,18 @@ async def _raise(ctx: Context, *, arg=None):
                     f"Sorry {ctx.author.display_name}, only Mods or Admins can clear the queue."
                 )
         case "next":
-            if len(q) < 1:
-                await ctx.send("Nobody else in the queue!")
-                return
-            print("popping next person from queue")
-            got = q.popleft()
-            await ctx.send(f"Next up: {got}")
+            current_roles = [r.name for r in ctx.author.roles]
+            if "Admin" in current_roles or "Mod" in current_roles:
+                if len(q) < 1:
+                    await ctx.send("Nobody else in the queue!")
+                    return
+                print("popping next person from queue")
+                got = q.popleft()
+                await ctx.send(f"Next up: {got}")
+            else:
+                await ctx.send(
+                    f"Sorry {ctx.author.display_name}, only Mods or Admins can get the next up person"
+                )
         case None:
             if ctx.author.display_name in q:
                 print(f"{ctx.author.display_name} already in the queue")
