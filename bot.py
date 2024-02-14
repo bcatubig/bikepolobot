4a9f5fdbb438e3111750b50e145067ca44504048
Brandon Catubig <bcatubig@pm.me>
2024-02-13 16:18:59 -0800

    refactor role check

--- bot.py
+++ bot.py
@@ -13,6 +13,14 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+def is_mod_or_admin(m: discord.Member) -> bool:
+    current_roles = [r.name for r in m.roles]
+
+    if "Admin" in current_roles or "Mod" in current_roles:
+        return True
+    return False
+
+
 @bot.command(name="raise")
 async def _raise(
     ctx: commands.context.Context,
@@ -28,51 +36,41 @@ async def _raise(
             msg += "- `!raise clear` - clear the queue -  _Admin/Mods only_\n"
             await ctx.send(msg)
         case "list":
-            print("Listing queue")
             if len(q) < 1:
                 await ctx.send("The queue is empty!")
             else:
                 msg = "Here is the current queue:\n"
 
                 for person in q:
-                    msg += f"- {person}\n"
+                    msg += f"- {person.mention}\n"
 
                 await ctx.send(msg)
         case "clear":
-            current_roles = [r.name for r in ctx.author.roles]
-
-            if "Admin" in current_roles or "Mod" in current_roles:
-                print("Clearing queue")
+            if is_mod_or_admin(ctx.author):
                 q.clear()
-                await ctx.send(f"Queue cleared, {ctx.author.display_name}")
+                await ctx.send(f"Queue cleared, {ctx.author.mention}")
             else:
                 await ctx.send(
                     f"Sorry {ctx.author.display_name}, only Mods or Admins can clear the queue."
                 )
         case "next":
-            current_roles = [r.name for r in ctx.author.roles]
-            if "Admin" in current_roles or "Mod" in current_roles:
+            if is_mod_or_admin(ctx.author):
                 if len(q) < 1:
                     await ctx.send("Nobody else in the queue!")
                     return
-                print("popping next person from queue")
                 got: discord.Member = q.popleft()
                 await ctx.send(f"Next up: {got.mention}")
             else:
                 await ctx.send(
-                    f"Sorry {ctx.author.display_name}, only Mods or Admins can get the next up person"
+                    f"Sorry {ctx.author.mention}, only Mods or Admins can get the next up person"
                 )
         case None:
             if ctx.author in q:
-                print(f"{ctx.author.display_name} already in the queue")
-                await ctx.send(
-                    f"{ctx.author.display_name}, you are already in the queue"
-                )
+                await ctx.send(f"{ctx.author.mention}, you are already in the queue")
                 return
 
-            print(f"Adding {ctx.author.display_name} to queue")
             q.append(ctx.author)
-            await ctx.send(f"{ctx.author.display_name} added to queue")
+            await ctx.send(f"{ctx.author.mention} added to queue")
 
 
 def main():
